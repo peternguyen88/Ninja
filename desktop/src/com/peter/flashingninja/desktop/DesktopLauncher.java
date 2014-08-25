348f08b32014ec36e7669e1c85fd923f7632da96
peternguyen88 <tronghieubk@gmail.com>
2014-08-25 09:39:19 +0100

    :lollipop: Added .gitattributes

---
+++ desktop/src/com/peter/flashingninja/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.peter.flashingninja.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.peter.flashingninja.FlashingNinja;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlashingNinja(), config);
+	}
+}
